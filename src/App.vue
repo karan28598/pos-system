efe2668ca7c039f8169c5376b79f613beefc071b
karan28598 <mr.karanagrawal@gmail.com>
2018-06-12 02:07:50 +0530

    migrate to vue2x

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <POS />
+  </div>
+</template>
+
+<script>
+import POS from './components/POS'
+
+export default {
+  name: 'App',
+  components: {
+    POS
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
